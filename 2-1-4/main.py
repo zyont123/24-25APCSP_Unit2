ab03ff8d503003a475dc2b027f59f762c644d982
zyon <zyon.taylor@vtsd.com>
2025-02-05 12:27:44 -0500

    g

--- 2-1-4/main.py
+++ 2-1-4/main.py
@@ -1,23 +1,68 @@
-#   a214_simple_window1.py
-#   A program creates a window on your screen using Tkinter.
-import tkinter as tk
+#   a212_cipher.py
+#   Rotation Cipher
 
-# main window
-root = tk.Tk()
-root.wm_geometry("200x200")
+MAX_KEY_SIZE = 26 
 
-# create empty frame
-frame_login = tk.Frame(root)
+def getMode():
+  while True:
+    print('Do you wish to encrypt or decrypt a message?')
+    mode = input().lower()
+    if mode in 'encrypt e decrypt d brute b'.split():
+      return mode
+    else:
+      print('Enter either "encrypt" or "e" or "decrypt" or "d" or "brute" or "b".')
 
-# activiate the grid in our new frames
-frame_login.grid()
 
-# widgets for frame login
-lbl_login = tk.Label(frame_login, text="Login")
-lbl_username.pack()
+def getMessage():
+  print('Enter your message:')
+  return input()
 
-# text box for username
-ent_username = tk.Entry(frame_login, bd=3)
-ent_username.pack()
+def getKey():
+  key = 0
+  while True:
+    print('Enter the key number (1-%s)' % (MAX_KEY_SIZE))
+    key = int(input())
+    if (key >= 1 and key <= MAX_KEY_SIZE):
+      return key
 
-root.mainloop()
+def getTranslatedMessage(mode, message, key):
+  if mode[0] == 'd':
+    key = -key
+  translated = ''
+
+  for symbol in message:
+    if symbol.isalpha():
+      num = ord(symbol)
+      num += key
+
+      if symbol.isupper():
+        if num > ord('Z'):
+          num -= 26
+        elif num < ord('A'):
+          num += 26
+      elif symbol.islower():
+        if num > ord('z'):
+          num -= 26
+        elif num < ord('a'):
+          num += 26
+    
+      translated += chr(num)
+    
+    else:
+      translated += symbol
+
+  return translated
+
+mode = getMode()
+message = getMessage()
+
+if mode[0] != 'b':
+  key = getKey()
+
+
+print('Your translated text is:')
+if mode[0] != 'b':
+  print(getTranslatedMessage(mode, message, key))
+else:
+  for key in range(1, MAX_KEY_SIZE + 1):
+    print(key, getTranslatedMessage('decrypt', message, key))
