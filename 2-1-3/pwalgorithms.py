428b51eadb5614744b766c1cedc2e34d56d0127e
zyon <zyon.taylor@vtsd.com>
2025-02-11 12:28:10 -0500

    p

---
+++ 2-1-3/pwalgorithms.py
@@ -0,0 +1,35 @@
+# Module pwalgorithms
+
+# get words from password dictionary file
+def get_dictionary():
+  words = []
+  dictionary_file = open("dictionary.txt")
+  for line in dictionary_file:
+    # store word, omitting trailing new-line
+    words.append(line[:-1])
+  dictionary_file.close()
+  return words
+
+# analyze a one-word password
+def one_word(password):
+  words = get_dictionary()
+  guesses = 0
+  # get each word from the dictionary file
+  for w in words:
+    guesses += 1
+    if (w == password):
+      return True, guesses
+  return False, guesses
+
+
+# analyze a one-word password
+def two_word(password):
+  words = get_dictionary()
+  guesses = 0
+  # get each word from the dictionary file
+  for w in words:
+   for h in words:
+    guesses += 1
+    if (w+h == password):
+      return True, guesses
+  return False, guesses
